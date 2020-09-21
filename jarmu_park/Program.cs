ff7a6d8832e6027e1e8f84788bb6b4ee2b790ada
SzklevanMilan <szklevanm@gmail.com>
2020-09-21 13:04:02 +0200

    Működő autó osztály

--- jarmu_park/Program.cs
+++ jarmu_park/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace jarmu_park
 {
@@ -10,6 +11,26 @@ namespace jarmu_park
     {
         static void Main(string[] args)
         {
+            Auto lada = new Auto("piros","Lada 2112",11);
+            Auto trabant = new Auto("zöld","Trabant 1.1",8);
+
+            lada.Tankol(20);
+            trabant.Tankol(20);
+
+            lada.Allapot();
+            trabant.Allapot();
+
+            Console.WriteLine("Mennyit megy a lada: ");
+            int km = int.Parse(Console.ReadLine());
+            lada.Megy(km);
+            lada.Allapot();
+
+            Console.WriteLine("Mennyit megy a trabant: ");
+            int km2 = int.Parse(Console.ReadLine());
+            trabant.Megy(km2);
+            trabant.Allapot();
+
+            Console.ReadKey();
         }
     }
 }
